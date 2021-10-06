7a4544c8ddc5f34402805ffe8821d1f037e14380
James Hamilton <james.hamilton@guardsquare.com>
2021-10-06 11:45:31 +0200

    Update year in KotlinSourcePrinter

--- src/main/java/proguard/kotlin/printer/KotlinSourcePrinter.java
+++ src/main/java/proguard/kotlin/printer/KotlinSourcePrinter.java
@@ -2,7 +2,7 @@
  * ProGuard -- shrinking, optimization, obfuscation, and preverification
  *             of Java bytecode.
  *
- * Copyright (c) 2002-2020 Guardsquare NV
+ * Copyright (c) 2002-2021 Guardsquare NV
  */
 package proguard.kotlin.printer;
 
